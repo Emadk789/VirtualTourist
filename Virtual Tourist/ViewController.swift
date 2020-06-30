679f7b7fb96b194102e61fc16b610cfb7e54dc94
emadAlbarnawi <emadk789@hotmail.com>
2020-06-30 18:06:10 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Emad Albarnawi on 30/06/2020.
+//  Copyright © 2020 Emad Albarnawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
