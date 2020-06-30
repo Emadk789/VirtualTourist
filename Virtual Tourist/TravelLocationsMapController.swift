635f60b72b5f7e495708e2ff81f66701c4060890
emadAlbarnawi <emadk789@hotmail.com>
2020-07-01 01:10:03 +0300

    feat: Add gesture recognizer to the TravelLocationsMapController to add annotations by tapping or long pressing on the map.

---
+++ Virtual Tourist/TravelLocationsMapController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Emad Albarnawi on 30/06/2020.
+//  Copyright © 2020 Emad Albarnawi. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import UIKit
+import MapKit
+
+class TravelLocationsMapController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager =  CLLocationManager()
+    
+    var annotations = [MKPointAnnotation]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        
+        gestureConfigurations();
+    }
+    
+}
+
+// MARK:- MKMapViewDelegate.
+extension TravelLocationsMapController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.open(URL(string: toOpen)!)
+            }
+        }
+    }
+    
+    
+    
+}
+
+// MARK:- Gesture Recognizer Methods.
+extension TravelLocationsMapController {
+    func gestureConfigurations() {
+        let singleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(TravelLocationsMapController.handelMapTab(_:)))
+        mapView.addGestureRecognizer(singleTapRecognizer)
+        
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(TravelLocationsMapController.handleLongPress(_:)))
+        longPressRecogniser.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(longPressRecogniser)
+    }
+    @objc func handelMapTab(_ gestureRecognizer:  UIGestureRecognizer){
+        addAnnotation(with: gestureRecognizer);
+    }
+    
+    @objc func handleLongPress(_ gestureRecognizer : UIGestureRecognizer){
+        if gestureRecognizer.state != .began { return }
+        addAnnotation(with: gestureRecognizer);
+    }
+    func addAnnotation(with gestureRecognizer: UIGestureRecognizer){
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let touchMapCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchMapCoordinate;
+        
+        annotations.append(annotation)
+        mapView.addAnnotation(annotation);
+    }
+}
